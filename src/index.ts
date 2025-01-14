1edfbfe4448534014068b773b31f3ec86f494c4a
leon <554638047@qq.com>
2025-01-14 18:02:13 +0800

    feat: params

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import nunjucks from 'nunjucks';
 
+type Opts = {
+    watch: boolean;
+    noCache: boolean;
+}
+
+type KoaParams = {
+    searchPaths: string;
+    opt: Opts
+}
+
 class KoaNunjucks {
     protected env;
-    constructor(params) {
-        
+    protected searchPaths = ;
+    protected opts;
+    constructor(params: KoaParams) {
+        this.searchPaths = params.searchPaths;
+        this.opts = params.opt;
         this.initialization()
     }
 
     initialization() {
-        this.env = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
+        this.env = new nunjucks.Environment(new nunjucks.FileSystemLoader(this.searchPaths));
     }
 
     render() {
@@ -16,9 +29,12 @@ class KoaNunjucks {
     }
 }
 
-const koaNunjucks = () => {
+const koaNunjucks = (searchPaths: string, opt: Opts) => {
     return async (cxt, next) => {
-        const koaNunjucksInstance = new KoaNunjucks({});
+        const koaNunjucksInstance = new KoaNunjucks({
+            searchPaths,
+            opt
+        });
         cxt.render = koaNunjucksInstance.render.bind(koaNunjucks)
         await next()
     }
